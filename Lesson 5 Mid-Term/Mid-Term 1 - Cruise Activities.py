a8c5700a7e25fe87b68893f541c949d222fbd609
Tristan-Kendall <tristankk.business@gmail.com>
2025-06-25 22:21:35 -0400

    Fixed excessive print() calls.

--- Lesson 5 Mid-Term/Mid-Term 1 - Cruise Activities.py
+++ Lesson 5 Mid-Term/Mid-Term 1 - Cruise Activities.py
@@ -15,45 +15,50 @@ while print_another == 'y':
 
     # Child Activities
     if cruiser_age >= 1 and cruiser_age <= 12:
-        print('All children ages 12 and under can enjoy the following activities:')
-        print('\tKids Club')
-        print('\tSplash Zone')
+        print('All children ages 12 and under can enjoy the following activities:\n' \
+        '\tKids Club\n' \
+        '\tSplash Zone'
+        )
         print()
 
     # Teenager Activities (13–17)
     elif cruiser_age >= 13 and cruiser_age <= 17:
-        print('All teenagers ages 13-19 can enjoy the following activities:')
-        print('\tTeen Club')
-        print('\tWater Slides')
+        print('All teenagers ages 13-19 can enjoy the following activities:\n' \
+        '\tTeen Club\n' \
+        '\tWater Slides'
+        )
         print()
 
     # Teen + Young Adult (18–19)
     elif cruiser_age == 18 or cruiser_age == 19:
-        print('All teenagers ages 13-19 can enjoy the following activities:')
-        print('\tTeen Club')
-        print('\tWater Slides')
-        print('All 18 and 19 year old passengers may also participate in:')
-        print('\tDance Club')
-        print('\tComedy Club')
-        print('\tAdult Pool')
+        print('All teenagers ages 13-19 can enjoy the following activities:\n' \
+        '\tTeen Club\n' \
+        '\tWater Slides\n' \
+        'All 18 and 19 year old passengers may also participate in:\n' \
+        '\tDance Club\n' \
+        '\tComedy Club\n' \
+        '\tAdult Pool\n' \
+        )
         print()
 
     # Young Adult (age 20)
     elif cruiser_age == 20:
-        print('All young adult passengers age 20 may enjoy the following activities:')
-        print('\tDance Club')
-        print('\tComedy Club')
-        print('\tAdult Pool')
+        print('All young adult passengers age 20 may enjoy the following activities:\n' \
+        '\tDance Club\n' \
+        '\tComedy Club\n' \
+        '\tAdult Pool'
+        )
         print()
 
     # Adult (21–64)
     elif cruiser_age >= 21 and cruiser_age < 65:
-        print('All adults 21 and older can enjoy the following activities:')
-        print('\tDance Club')
-        print('\tComedy Club')
-        print('\tAdult Pool')
-        print('\tAll bars and casinos')
-        print('As a reminder, please drink responsibly!')
+        print('All adults 21 and older can enjoy the following activities:\n' \
+        '\tDance Club\n' \
+        '\tComedy Club\n' \
+        '\tAdult Pool\n' \
+        '\tAll bars and casinos' \
+        'As a reminder, please drink responsibly!'
+        )
         print()
     
     elif cruiser_age <= 0 or cruiser_age >= 120:
@@ -62,13 +67,14 @@ while print_another == 'y':
 
     # Senior (65+)
     else:
-        print('All adults 21 and older can enjoy the following activities:')
-        print('\tDance Club')
-        print('\tComedy Club')
-        print('\tAdult Pool')
-        print('\tAll bars and casinos')
-        print('Senior cruisers are entitled to a 15% purchase discount on-board')
-        print('As a reminder, please drink responsibly!')
+        print('All adults 21 and older can enjoy the following activities:\n' \
+        '\tDance Club\n' \
+        '\tComedy Club\n' \
+        '\tAdult Pool\n' \
+        '\tAll bars and casinos\n' \
+        'Senior cruisers are entitled to a 15% purchase discount on-board' \
+        'As a reminder, please drink responsibly!'
+        )
         print()
 
     print_another = input('Would you like to print another activity list for a new passenger? y or n? ')
